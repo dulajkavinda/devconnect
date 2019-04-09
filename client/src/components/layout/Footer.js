67d04558afc107aa1baee56e2d557d2b6187b57d
dulajkavinda <dulajkaavinda@gmail.com>
2019-04-09 14:52:48 +0530

    adding react to build the front-end

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; 2018 Dev Connector
+    </footer>
+  );
+}
